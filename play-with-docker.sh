b4e684c1e0a8f61d5574327fbabe182cde657a22
Humberto Dias <humberto.dias@ubisoft.com>
2020-07-06 23:02:07 +0200

    MINOR - Api now is a subproject

--- play-with-docker.sh
+++ play-with-docker.sh
@@ -4,7 +4,7 @@ apk add openjdk11
 
 ./gradlew microBundle
 
-java -jar build/libs/ROOT-microbundle.jar \
+java -jar dunity-api/build/libs/ROOT-microbundle.jar \
 --autoBindSsl \
 --nocluster \
 --nohostaware
